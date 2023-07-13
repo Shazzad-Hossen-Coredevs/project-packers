6968b4ce06da1bc6e69b4492129bdf5ea6060198
rafi-coredevs <rafi_uddin@coredevs.ltd>
2023-07-13 13:02:07 +0600

    fixed issues

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
-
+import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.svg";
+import Button from "../Ui Elements/Buttons/Button";
 import Input from "../Ui Elements/Input/Input";
 const Header = () => {
   return (
-    <div className="navbar py-[25px] flex align-middle ">
+    <div className="container mx-auto navbar py-[25px] flex items-center justify-between ">
       <div className="w-[142px] h-11 justify-start items-center gap-2 inline-flex">
         <img src={Logo} alt="Project Packers" />
         <div className="text-secondary text-base font-bold leading-none">
@@ -12,7 +13,23 @@ const Header = () => {
           Packers
         </div>
       </div>
-      <Input type="text" placeholder="Paste the URL of the product" />
+      <Input
+        icon="searchYellow"
+        type="text"
+        placeholder="Paste the URL of the product"
+      />
+      <div className="flex gap-10 items-center">
+        <Link
+          className="align-center font-sans font-normal text-secondary h-fit"
+          to="..."
+        >
+          Support
+        </Link>
+        <div className="flex gap-2 items-center">
+          <Button text="Login" />
+          <Button text="Sign Up" secondary />
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Ui Elements/Input/Input.jsx
+++ src/Components/Ui Elements/Input/Input.jsx
@@ -1,11 +1,15 @@
+import Icon from "../Icon/Icon";
 
 
-const Input = ({type, placeholder}) => {
+const Input = ({type, placeholder, icon}) => {
   return (
-    <div className="flex align-middle  border border-secondary">
 
-        <input className="" type={type} placeholder={placeholder} />
+
+    <div className="flex items-center w-[700px] gap-[10px] border border-secondary rounded-[50px] p-[10px]">
+        <Icon name={icon} />
+        <input className="rounded-[50px] w-full font-sans font-medium focus:outline-none placeholder:text-secondary px-2 " type={type} placeholder={placeholder} />
     </div>
+  
   );
 };
 

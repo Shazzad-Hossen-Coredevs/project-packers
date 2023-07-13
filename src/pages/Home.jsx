5870cf5c5e1786bdbd88dad5c00a65ba19d7c024
Shazzad-Hossen-Coredevs <shazzad_hossen@coredevs.ltd>
2023-07-13 13:09:08 +0600

    Layout changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,9 @@
-import Footer from "../Components/Footer/Footer";
-import Header from "../Components/Header/Header";
-
 
 const Home = () => {
 
     return (
-        <div className="">
-            <Header />
-
-           <Footer/>
+        <div>
+          
 
         </div>
     );

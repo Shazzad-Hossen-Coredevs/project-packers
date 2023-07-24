0d5cdb6f866cef45f810377045d680a0df811e23
rafi-coredevs <rafi_uddin@coredevs.ltd>
2023-07-24 16:01:08 +0600

    price card complete

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,10 +1,21 @@
 import PriceCard from "../Components/PriceCard/PriceCard";
 
-
+const data = [
+    {
+        id: 1,
+        title: "Iphone 14 max pro",
+        price: 1099
+},{
+    id: 1,
+        title: "Iphone 14 max pro",
+        price: 1099
+}]
 const Test = () => {
     return (
-   <div className="container mx-auto">
-   <PriceCard />
+   <div className="container mx-auto ">
+   <PriceCard type="product" origin="United States of America" source='apple.com' price={450} arrival="1 - 2 Weeks" />
+   <PriceCard type="cart" sellerTakes={22590} />
+   <PriceCard type="checkout" products={data} />
    </div>
     );
 };

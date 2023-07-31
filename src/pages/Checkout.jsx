8022e6ad86dc9f8a455544b6349b056ce0d21d4b
rafi-coredevs <rafi_uddin@coredevs.ltd>
2023-07-31 11:35:37 +0600

    checkout page complete

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,5 @@
 import PriceCard from "../Components/PriceCard/PriceCard";
+import Input from '../Components/UiElements/Input/Input'
 const cartItems = [
     {
       id: 1,
@@ -24,7 +25,33 @@ const Checkout = () => {
         <div className="container mx-auto py-12 ">
       <div className="grid grid-cols-5 gap-8">
         <div className="col-span-5 sm:col-span-3 px-5 sm:px-0">
-          
+          <div className="mb-6">
+            <h3 className="text-secondary font-semibold mb-6">Contact Information</h3>
+            <div className="grid gap-4">
+              <Input styles='primary' type="email" label="Email Address." placeholder="Enter your Email Address." border />
+              <Input styles='primary' type="tel" label="Phone Number." placeholder="Enter your Phone Number" border required />
+              <Input styles='primary' type="tel" label="Alternative phone number (Optional)." placeholder="Enter your Phone Number" border />
+            </div>
+          </div>
+          <div className="">
+            <h3 className="text-secondary font-semibold mb-6">Shipping Address.</h3>
+            <div className="grid gap-4">
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <Input styles="primary" type='text' label="First Name" placeholder="Enter your first name." border />
+                <Input styles="primary" type='text' label="Last Name" placeholder="Enter your last name." border />
+              </div>
+              <Input styles='primary' type="text" label="Address" placeholder="Enter your Address." border />
+              <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+              <Input styles='primary' type="text" label="City" placeholder="City" border />
+              <Input styles='primary' type="text" label="Area" placeholder="Area" border />
+              <Input styles='primary' type="number" label="Zip Code" placeholder="Zip Code" border />
+              </div>
+              <div className="w-full">
+              <label className="block pb-3">Delivery Instructions (Optional)</label>
+              <textarea className="border  w-full text-secondary placeholder:text-secondary p-2" placeholder="Write Here..." rows={8}  />
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-span-5 sm:col-span-2">
           <PriceCard
